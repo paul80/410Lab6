2012a25d892801486e2f41b76dbdd944694b2476
paul80 <paulnhan26@gmail.com>
2015-02-24 10:51:51 -0700

    lab6 not done yet

---
+++ 2/bookmarks/main/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.shortcuts import redirect
+
+from main.models import Link
+from main.models import Tag
+
+def index(request):
+	context=RequestContext(request)
+
+	links=Link.objects.all()
+
+	return render_to_response('main/index.html', {'links':links},context)
+
+def tags(request):
+	context = RequestContext(request)
+	tags = Tag.objects.all()
+	return render_to_response('main/tags.html', {'tags': tags}, context)
+
+def tag(request, tag_name):
+	context = RequestContext(request)
+	the_tag = Tag.objects.get(name=tag_name)
+	links = the_tag.link_set.all()
+	return render_to_response('main/index.html', {'links': links, 'tag_name': '#' + tag_name}, context)
+
+def add_link(request):
+	context= RequestContext(request)
+	if request.method =="POST":
+		url=request.POST.get('url','')
+		tags=request.POST.get('tags','')
+		title= request.POST.get('title','')
+
+		new_link= Link(title=title,url=url,tags=tags)
+		new_link.save()
+
+
+	return redirect(index)
+
+# Create your views here.
+#http://127.0.0.1:8000/bookmarks/
